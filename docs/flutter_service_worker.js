515f81f7231f696e819956b0070fdf52cc9a81a8
Hugo Seong <hugo.se@worldshoppe.com>
2026-05-19 11:00:16 -0400

    Force web cache refresh on deploy

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -8,24 +8,28 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     (async () => {
       try {
+        const keys = await caches.keys();
+        await Promise.all(keys.map((key) => caches.delete(key)));
+      } catch (e) {
+        console.warn('Failed to clear app caches:', e);
+      }
+
+      try {
         await self.registration.unregister();
       } catch (e) {
         console.warn('Failed to unregister the service worker:', e);
       }
 
       try {
-        const clients = await self.clients.matchAll({
-          type: 'window',
-        });
-        // Reload clients to ensure they are not using the old service worker.
+        const clients = await self.clients.matchAll({ type: 'window' });
         clients.forEach((client) => {
           if (client.url && 'navigate' in client) {
             client.navigate(client.url);
           }
         });
       } catch (e) {
-        console.warn('Failed to navigate some service worker clients:', e);
+        console.warn('Failed to refresh service worker clients:', e);
       }
-    })()
+    })(),
   );
 });
